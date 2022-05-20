07e10c4d5918ffa23f4cd3b86c9ee0383f5801aa
Yucheng Huang <yuh032@ucsd.edu>
2022-05-19 23:53:24 -0700

    [160] easy: intersection of two linked lists: hacked with set

--- 160.intersection-of-two-linked-lists.py
+++ 160.intersection-of-two-linked-lists.py
@@ -1,23 +1,34 @@
 #
-# @lc app=leetcode id=160 lang=python3
+# @lc app=leetcode id=160 lang=python
 #
 # [160] Intersection of Two Linked Lists
 #
 
 # @lc code=start
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        ptr1 = headA
-        ptr2 = headB
-        while ptr1 != ptr2:
-            ptr1 = ptr1.next if ptr1 else headB
-            ptr2 = ptr2.next if ptr2 else headA
-        return ptr1
+
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        hashSetA = set()
+        listA = headA
+        listB = headB
+        while listA:
+            hashSetA.add(listA)
+            listA = listA.next
+        while listB:
+            if listB in hashSetA:
+                return listB
+            listB = listB.next
+
 # @lc code=end
 
