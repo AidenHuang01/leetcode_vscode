b3c4472ba970842400350820cdcc32186b7d601d
Yucheng Huang <yuh032@ucsd.edu>
2022-05-26 16:34:19 -0700

    [35] search insert position: 2nd solve

--- 35.search-insert-position.py
+++ 35.search-insert-position.py
@@ -1,29 +1,22 @@
 #
-# @lc app=leetcode id=35 lang=python
+# @lc app=leetcode id=35 lang=python3
 #
 # [35] Search Insert Position
 #
-
+from typing import List
 # @lc code=start
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        # implement using a binary search
-        left = 0
-        right = len(nums)
-        while left < right: 
-            mid = (left + right) // 2
-            if nums[mid] < target:
-                left = mid + 1
-            elif nums[mid] > target:
-                right = mid
-            else:
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
                 return mid
-        return left 
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid -1
+        return l
         
 # @lc code=end
 
