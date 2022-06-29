d001c194325d16425bfb7dac479b1dae13531887
Yucheng Huang <yuh032@ucsd.edu>
2022-06-29 16:01:03 -0700

    [39] medium : combination sum : 3q

--- 39.combination-sum.py
+++ 39.combination-sum.py
@@ -1,5 +1,5 @@
 #
-# @lc app=leetcode id=39 lang=python
+# @lc app=leetcode id=39 lang=python3
 #
 # [39] Combination Sum
 #
@@ -12,41 +12,24 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        def search(candidates, target, temp, result):
-            if target < 0:
+        res = []
+        track = []
+        track_sum = [0]
+        candidates.sort()
+        def backtrack(nums, start, target):
+            if track_sum[0] == target:
+                res.append(track.copy())
+            if track_sum[0] > target:
                 return
-            if target == 0:
-                result.append(temp)
-            for i in range(len(candidates)):
-                temp.append(candidates[i])
-                search(candidates[i:], target - candidates[i], temp, result)
-                temp.pop(-1)
-        result = []
-        temp = []
-        search(candidates, target, temp, result)
-        return result
+            for i in range(start, len(nums)):
+                track.append(nums[i])
+                track_sum[0] += nums[i]
+                backtrack(nums, i, target)
+                track_sum[0] -= track.pop()
 
-        result = []
-        curr = []
-        self.backTrack(candidates, target, curr, result)
-        return result
-
-
-    def backTrack(self, candidates, target, curr, result):
-        # base case
-        if target < 0:
-            return
-        if target == 0:
-            result.append(curr[:])
-            return
-            
-        for i in range(len(candidates)):
-            # make decision
-            curr.append(candidates[i])
-            # forward
-            self.backTrack(candidates[i:], target-candidates[i], curr, result)
-            # cancle decision
-            curr.pop(-1)
+                
+        backtrack(candidates, 0, target)
+        return res
 
         
 
